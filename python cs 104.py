25bbf76cedecc2780bb805840c50269617596b82
s1296527 <55250917+s1296527@users.noreply.github.com>
2019-09-19 07:32:24 -0400

    Add files via upload

---
+++ python cs 104.py
@@ -0,0 +1,9 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> p1 = 1
+>>> p2 = 2
+>>> p5 = 5
+>>> p10 = 10
+>>> if (p1>p2) :
+	print ("yes")
+else : ("no")
